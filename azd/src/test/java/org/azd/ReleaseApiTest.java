881e186ace90a2c72acfa288a92084fa02c6043e
Harish <harish.karthic@gmail.com>
2021-06-18 06:36:32 +0100

    Add tests for ReleaseApi

--- azd/src/test/java/org/azd/ReleaseApiTest.java
+++ azd/src/test/java/org/azd/ReleaseApiTest.java
@@ -1,8 +1,11 @@
 package org.azd;
 
+import org.azd.build.BuildApi;
+import org.azd.enums.SingleReleaseExpands;
 import org.azd.exceptions.AzDException;
 import org.azd.exceptions.DefaultParametersException;
 import org.azd.helpers.JsonMapper;
+import org.azd.interfaces.BuildDetails;
 import org.azd.interfaces.ReleaseDetails;
 import org.azd.release.ReleaseApi;
 import org.azd.utils.AzDDefaultParameters;
@@ -14,6 +17,7 @@ import java.io.File;
 public class ReleaseApiTest {
     private static final JsonMapper MAPPER = new JsonMapper();
     private static ReleaseDetails r;
+    private static BuildDetails b;
 
     @Before
     public void init() throws AzDException {
@@ -25,10 +29,29 @@ public class ReleaseApiTest {
         String project = m.getP();
         AzDDefaultParameters defaultParameters = new AzDDefaultParameters(organization, project, token);
         r = new ReleaseApi(defaultParameters);
+        b = new BuildApi(defaultParameters);
     }
 
     @Test
-    public void shouldGetLogsFromGivenRelease() throws DefaultParametersException, AzDException {
+    public void shouldCreateReleasePipeline() throws DefaultParametersException, AzDException {
+        var build = b.getBuild(176);
+        r.createRelease(2, "Sample Release", "_Demo-CI", build.getBuildNumber(),
+                "Demo-CI", false, "none");
+    }
 
+    @Test
+    public void shouldGetAllReleases() throws DefaultParametersException, AzDException {
+        r.getReleases();
+    }
+
+    @Test
+    public void shouldGetARelease() throws DefaultParametersException, AzDException {
+        r.getRelease(4);
+    }
+
+    @Test
+    public void shouldGetReleaseEnvironmentDetails() throws DefaultParametersException, AzDException {
+        var res = r.getRelease(4, SingleReleaseExpands.TASKS);
+        r.getReleaseEnvironment(res.getId(), res.getEnvironments().stream().findFirst().get().getId());
     }
 }
