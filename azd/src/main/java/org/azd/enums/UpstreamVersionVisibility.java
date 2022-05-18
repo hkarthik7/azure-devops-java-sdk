301eb37ee56eeb2ffb91b5aff4e0d00020a47839
JIHUN KIM <jihunkimkw@gmail.com>
2022-05-18 21:48:58 +0900

    Add/Change Maven API Enum class
    - Add PackagesBatchOperation Enum for /packagesbatch of all artifact packages
    - Change Enum class to UpstreamVersionVisibiliity.
      UpstreamVersionVisibiliity is Enum class with two variable.

---
+++ azd/src/main/java/org/azd/enums/UpstreamVersionVisibility.java
@@ -0,0 +1,10 @@
+package org.azd.enums;
+
+
+/***
+ * Indicates whether external upstream versions should be considered for this
+ * package
+ */
+public enum UpstreamVersionVisibility {
+    ALLOWEXTERNALVERSIONS, AUTO
+}
