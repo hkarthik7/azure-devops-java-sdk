a82df0a60a83ca346f4b8133d5dbc200aaec86e3
Harish <harish.karthic@gmail.com>
2022-07-20 17:50:47 +0100

    Updated comments for StreamHelper

--- azd/src/main/java/org/azd/helpers/StreamHelper.java
+++ azd/src/main/java/org/azd/helpers/StreamHelper.java
@@ -7,16 +7,33 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class StreamHelper {
+    /**
+     * Default size of byte array
+     */
     private static int DEFAULT_BYTE_ARRAY_SIZE = 8192;
 
+    /**
+     * Returns the default byte array size
+     * @return Default byte array size
+     */
     public static int getDefaultByteArraySize() {
         return DEFAULT_BYTE_ARRAY_SIZE;
     }
 
+    /**
+     * Sets the byte array size
+     * @param defaultByteArraySize Size of the byte array to set
+     */
     public static void setDefaultByteArraySize(int defaultByteArraySize) {
         DEFAULT_BYTE_ARRAY_SIZE = defaultByteArraySize;
     }
 
+    /**
+     * Downloads the contents from InputStream and saves it in a file.
+     * @param fileName Pass the full path with file name to save the contents.
+     * @param responseStream Input response stream from Api or from a file.
+     * @throws AzDException Default exception handler
+     */
     public static void download(String fileName, InputStream responseStream) throws AzDException {
         try (FileOutputStream outputStream = new FileOutputStream(fileName, false)) {
             int read;
@@ -29,6 +46,12 @@ public class StreamHelper {
         }
     }
 
+    /**
+     * Convert the InputStream stream to string
+     * @param responseStream Input response stream from Api or from a file.
+     * @return String content
+     * @throws AzDException Default exception handler
+     */
     public static String convertToString(InputStream responseStream) throws AzDException {
         try {
             return new String(responseStream.readAllBytes(), StandardCharsets.UTF_8);
@@ -37,6 +60,12 @@ public class StreamHelper {
         }
     }
 
+    /**
+     * Convert the file contents to input stream
+     * @param file File object specified with complete path
+     * @return Input stream
+     * @throws AzDException Default exception handler
+     */
     public static InputStream convertToStream(File file) throws AzDException {
         try {
             return new DataInputStream(new FileInputStream(file));
@@ -45,8 +74,15 @@ public class StreamHelper {
         }
     }
 
+    /**
+     * Convert the string content to input stream
+     * @param content String content to convert
+     * @return Input stream
+     * @throws AzDException Default exception handler
+     */
     public static InputStream convertToStream(String content) throws AzDException {
         try {
+            if (content.isEmpty()) throw new AzDException("Content cannot be empty.");
             return new ByteArrayInputStream(content.getBytes());
         } catch (Exception e) {
             throw new AzDException(ApiExceptionTypes.InvalidArgumentException.name(), e.getMessage());
