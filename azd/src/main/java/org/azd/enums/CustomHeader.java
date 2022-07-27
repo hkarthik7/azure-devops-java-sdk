ffc93022ed3b8e3a157a0c992e1882980d5c7870
Harish <harish.karthic@gmail.com>
2022-07-27 06:59:19 +0100

    Added custom headers

--- azd/src/main/java/org/azd/enums/CustomHeader.java
+++ azd/src/main/java/org/azd/enums/CustomHeader.java
@@ -1,12 +1,52 @@
 package org.azd.enums;
 
+/**
+ * Custom header type to set headers for request/response
+ */
 public enum CustomHeader {
+    /**
+     * Json content type applicable for most of the post request.
+     * "Content-Type", "application/json"
+     */
     JSON_CONTENT_TYPE("Content-Type", "application/json"),
+    /**
+     * Json content to accept
+     * "Accept", "application/json"
+     */
     JSON("Accept", CustomHeader.JSON_CONTENT_TYPE.value),
+    /**
+     * Should be used for post or patch operations
+     * "Content-Type", "application/json-patch+json"
+     */
     JSON_PATCH(CustomHeader.JSON_CONTENT_TYPE.name, "application/json-patch+json"),
+    /**
+     * To accept the plain text response
+     * "Accept", "text/plain"
+     */
     TEXT_CONTENT(CustomHeader.JSON.name, "text/plain"),
-    URL_ENCODED(JSON_CONTENT_TYPE.name, "application/x-www-form-urlencoded"),
-    STREAM(JSON_CONTENT_TYPE.name, "application/octet-stream");
+    /**
+     * For url encoded request/response
+     * "Content-Type", "application/x-www-form-urlencoded"
+     */
+    URL_ENCODED(CustomHeader.JSON_CONTENT_TYPE.name, "application/x-www-form-urlencoded"),
+    /**
+     * Should be set for stream upload
+     * "Content-Type", "application/octet-stream"
+     */
+    STREAM(CustomHeader.JSON_CONTENT_TYPE.name, "application/octet-stream"),
+    /**
+     * Should be set for stream upload/download as zip content
+     * "Content-Type", "application/zip"
+     */
+    STREAM_ZIP(CustomHeader.JSON_CONTENT_TYPE.name, "application/zip"),
+    /**
+     * Should be set to accept the stream content from Api as zip
+     */
+    STREAM_ZIP_ACCEPT(CustomHeader.JSON.name, CustomHeader.STREAM_ZIP.getValue()),
+    /**
+     * Should be set to accept the stream content from Api
+     */
+    STREAM_ACCEPT(CustomHeader.JSON.name, CustomHeader.STREAM.getValue());
 
     private String name;
     private String value;
