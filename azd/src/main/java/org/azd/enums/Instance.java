2537d0b53cf631876a9d514b8bddbee92377bb27
Harish <harish.karthic@gmail.com>
2022-07-27 06:59:05 +0100

    Updated comments

--- azd/src/main/java/org/azd/enums/Instance.java
+++ azd/src/main/java/org/azd/enums/Instance.java
@@ -1,7 +1,16 @@
 package org.azd.enums;
 
+/**
+ * Base Api instance
+ */
 public enum Instance {
+    /**
+     * Azure DevOps Api base instance
+     */
     BASE_INSTANCE("https://dev.azure.com/"),
+    /**
+     * Accounts Api instance
+     */
     ACCOUNT_INSTANCE("https://app.vssps.visualstudio.com");
 
     private String instance;
