b2720f701936be6ea5e87f2135aca58e3b6d2b0d
Harish <harish.karthic@gmail.com>
2022-08-07 18:02:12 +0100

    Resource Ids initial commit

---
+++ azd/src/main/java/org/azd/common/ResourceId.java
@@ -0,0 +1,42 @@
+package org.azd.common;
+
+/**
+ * Collection of resource area identifiers.
+ */
+public abstract class ResourceId {
+    public static final String ACCOUNT = "0d55247a-1c47-4462-9b1f-5e2125590ee6";
+    public static final String BUILD = "5d6898bb-45ec-463f-95f9-54d49c71752e";
+    public static final String COLLECTION = "79bea8f8-c898-4965-8c51-8bbc3966faa8";
+    public static final String CORE = "79134c72-4a58-4b42-976c-04e7115f32bf";
+    public static final String DASHBOARD = "31c84e0a-3ece-48fd-a29d-100849af99ba";
+    public static final String DELEGATED_AUTH = "a0848fa1-3593-4aec-949c-694c73f4c4ce";
+    public static final String DISCUSSION = "6823169a-2419-4015-b2fd-6fd6f026ca00";
+    public static final String DISTRIBUTED_TASK = "a85b8835-c1a1-4aac-ae97-1c3d0ba72dbd";
+    public static final String DROP = "7bf94c77-0ce1-44e5-a0f3-263e4ebbf327";
+    public static final String EXTENSION_MANAGEMENT = "6c2b0933-3600-42ae-bf8b-93d4f7e83594";
+    public static final String FAVORITE = "67349c8b-6425-42f2-97b6-0843cb037473";
+    public static final String GIT = "4e080c62-fa21-4fbc-8fef-2a10a2b38049";
+    public static final String GRAPH = "bb1e7ec9-e901-4b68-999a-de7012b920f8";
+    public static final String MAVEN = "6f7f8c07-ff36-473c-bcf3-bd6cc9b6c066";
+    public static final String MEMBER_ENTITLEMENT_MANAGEMENT = "68ddce18-2501-45f1-a17b-7931a9922690";
+    public static final String NUGET = "b3be7473-68ea-4a81-bfc7-9530baaa19ad";
+    public static final String NPM = "4c83cfc1-f33a-477e-a789-29d38ffca52e";
+    public static final String PACKAGE = "45fb9450-a28d-476d-9b0f-fb4aedddff73";
+    public static final String PACKAGING = "7ab4e64e-c4d8-4f50-ae73-5ef2e21642a5";
+    public static final String PIPELINES = "2e0bf237-8973-4ec9-a581-9c3d679d1776";
+    public static final String POLICY = "fb13a388-40dd-4a04-b530-013a739c72ef";
+    public static final String PROFILE = "8ccfef3d-2b87-4e99-8ccb-66e343d2daa8";
+    public static final String RELEASE = "efc2f575-36ef-48e9-b672-0c6fb4a48ac5";
+    public static final String REPORTING = "57731fdf-7d72-4678-83de-f8b31266e429";
+    public static final String SEARCH = "ea48a0a1-269c-42d8-b8ad-ddc8fcdcf578";
+    public static final String TEST = "3b95fb80-fdda-4218-b60e-1052d070ae6b";
+    public static final String TEST_RESULTS = "c83eaf52-edf3-4034-ae11-17d38f25404c";
+    public static final String TFVC = "8aa40520-446d-40e6-89f6-9c9f9ce44c48";
+    public static final String TOKEN = "af68438b-ed04-4407-9eb6-f1dbae3f922e";
+    public static final String USER = "970aa69f-e316-4d78-b7b0-b7137e47a22c";
+    public static final String WIT = "5264459e-e5e0-4bd8-b118-0985e68a4ec5";
+    public static final String WIKI = "bf7d82a0-8aa5-4613-94ef-6172a5ea01f3";
+    public static final String WORK = "1d4f49f9-02b9-4e26-b826-2cdb6195f2a9";
+    public static final String WORK_TRACKING = "85f8c7b6-92fe-4ba6-8b6d-fbb67c809341";
+    public static final String U_PACK = "d397749b-f115-4027-b6dd-77a65dd10d21";
+}
