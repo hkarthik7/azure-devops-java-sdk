f24adce38db9a626c35f673cd01a403c3c0aefdc
Harish <harish.karthic@gmail.com>
2022-08-10 10:02:25 +0100

    Added Api version for Wiki attachments

--- azd/src/main/java/org/azd/common/ApiVersion.java
+++ azd/src/main/java/org/azd/common/ApiVersion.java
@@ -44,6 +44,7 @@ public abstract class ApiVersion {
     public static final String SERVICE_HOOKS = "7.1-preview.1";
     public static final String VARIABLE_GROUPS = "7.1-preview.2";
     public static final String WIKI = "7.1-preview.2";
+    public static final String WIKI_ATTACHMENTS = "7.1-preview.1";
     public static final String WORK = "7.1-preview.1";
     public static final String WORK_ITEM_MIGRATE = "7.1-preview.1";
     public static final String WORK_ITEM_TRACKING = "7.1-preview.3";
