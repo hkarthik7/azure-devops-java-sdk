37f5a4d35a1d1597fec8f2870957bd6cfce1018c
Kurt Niemi <kurtn718@gmail.com>
2023-04-10 21:41:36 -0400

    Fix parsing exception
    Fix parsing condition if we have a Release Definition that contains an Environment that has a Condition.
    
    Closes #56

--- azd/src/main/java/org/azd/release/types/Condition.java
+++ azd/src/main/java/org/azd/release/types/Condition.java
@@ -8,6 +8,7 @@ package org.azd.release.types;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.azd.common.types.BaseAbstractMethod;
+import org.azd.enums.ConditionType;
 
 /**
  * Gets or sets the trigger type. 
