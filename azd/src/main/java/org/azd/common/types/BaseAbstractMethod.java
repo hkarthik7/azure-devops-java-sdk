1e6a8d29ad069bec4febfc897907e46fb9253c1b
Harish Karthic <harish.karthic@gmail.com>
2023-06-16 10:44:24 +0100

    getContinuationToken method for retrieving x-ms-continuationtoken response header value

--- azd/src/main/java/org/azd/common/types/BaseAbstractMethod.java
+++ azd/src/main/java/org/azd/common/types/BaseAbstractMethod.java
@@ -3,10 +3,15 @@ package org.azd.common.types;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.azd.exceptions.AzDException;
 import org.azd.helpers.JsonMapper;
+import org.azd.utils.RestClient;
 
 public abstract class BaseAbstractMethod {
     private final JsonMapper MAPPER = new JsonMapper();
 
+    /**
+     * toString() method to convert the POJO to string.
+     * @return String result.
+     */
     public String toString() {
         String res = null;
 
@@ -17,6 +22,10 @@ public abstract class BaseAbstractMethod {
         return res;
     }
 
+    /**
+     * Helper method to convert the string to Json node.
+     * @return JsonNode object.
+     */
     public JsonNode toJsonTree() {
         JsonNode res = null;
         try {
@@ -24,5 +33,15 @@ public abstract class BaseAbstractMethod {
         } catch (AzDException e) { }
 
         return res;
+    }
+
+    /**
+     * Retrieves the continuation token from response header. This can then be used for getting the paginated response.
+     * Note that not all the Apis return continuation token in the headers. If the header value "x-ms-continuationtoken"
+     * is present then it will be returned or an empty string will be returned.
+     * @return String value.
+     */
+    public String getContinuationToken() {
+        return RestClient.Metadata.getResponseHeader("x-ms-continuationtoken");
     }
 }
