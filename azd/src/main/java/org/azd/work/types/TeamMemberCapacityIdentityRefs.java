ec02f874a103992e9dcd9e313a62375b14c14ee6
Harish Karthic <harish.karthic@gmail.com>
2023-07-25 22:14:40 +0100

    TeamMemberCapacityIdentityRefs definition initial commit

---
+++ azd/src/main/java/org/azd/work/types/TeamMemberCapacityIdentityRefs.java
@@ -0,0 +1,24 @@
+package org.azd.work.types;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.azd.common.types.BaseAbstractMethod;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TeamMemberCapacityIdentityRefs extends BaseAbstractMethod {
+    /**
+     * Represents a collection of team member capacity identity references.
+     */
+    @JsonProperty("value")
+    private List<TeamMemberCapacityIdentityRef> teamMemberCapacityIdentityRefs;
+
+    public List<TeamMemberCapacityIdentityRef> getTeamMemberCapacityIdentityRefs() {
+        return teamMemberCapacityIdentityRefs;
+    }
+
+    public void setTeamMemberCapacityIdentityRefs(List<TeamMemberCapacityIdentityRef> teamMemberCapacityIdentityRefs) {
+        this.teamMemberCapacityIdentityRefs = teamMemberCapacityIdentityRefs;
+    }
+}
