b49a634a6d1f28cc30237504ff74f83e7329dfab
Harish Karthic <harish.karthic@gmail.com>
2023-07-26 05:36:16 +0100

    PropertiesCollection class for `GitPullRequestStatus`

---
+++ azd/src/main/java/org/azd/git/types/PropertiesCollection.java
@@ -0,0 +1,26 @@
+package org.azd.git.types;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.azd.common.types.BaseAbstractMethod;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PropertiesCollection extends BaseAbstractMethod {
+    /**
+     * The count of properties in the collection.
+     */
+    @JsonProperty("count")
+    private int count;
+    @JsonProperty("item")
+    private Object item;
+    /**
+     * The set of keys in the collection.
+     */
+    @JsonProperty("keys")
+    private String[] keys;
+    /**
+     * The set of values in the collection.
+     */
+    @JsonProperty("values")
+    private String[] values;
+}
