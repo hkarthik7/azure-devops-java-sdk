11f405f29c0540f00b81f7bce73dd17d8194b7cf
Harish Karthic <harish.karthic@gmail.com>
2023-08-05 08:01:41 +0100

    Update scope parameter of `getAuthorizationEndpoint` to use VsoScope

--- azd/src/main/java/org/azd/oauth/OAuthAccessTokenBuilder.java
+++ azd/src/main/java/org/azd/oauth/OAuthAccessTokenBuilder.java
@@ -3,6 +3,7 @@ package org.azd.oauth;
 import org.azd.enums.CustomHeader;
 import org.azd.enums.Instance;
 import org.azd.enums.RequestMethod;
+import org.azd.enums.VsoScope;
 import org.azd.exceptions.AzDException;
 import org.azd.helpers.URLHelper;
 import org.azd.http.RequestInformation;
@@ -11,6 +12,8 @@ import org.azd.oauth.types.AuthorizedToken;
 import org.azd.utils.BaseRequestBuilder;
 
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Provides the functionality to generate and oauth access token to call Azure DevOps Api.
@@ -34,24 +37,31 @@ public class OAuthAccessTokenBuilder extends BaseRequestBuilder {
      * @param redirectUrl Callback URL for your app. Must exactly match the URL registered with the app.
      * @return The authorization endpoint to authorize your app
      */
-    public String getAuthorizationEndpoint(String clientId, String state, String scope, String redirectUrl) {
+    public String getAuthorizationEndpoint(final String clientId, final String state, final List<VsoScope> scope, final String redirectUrl) {
+        Objects.requireNonNull(clientId);
+        Objects.requireNonNull(state);
+        Objects.requireNonNull(scope);
+        Objects.requireNonNull(redirectUrl);
 
-        var queryString = new LinkedHashMap<String, Object>() {{
-            put("response_type", "Assertion");
-            put("state", state);
-            put("scope", URLHelper.encodeSpace(scope));
-            put("redirect_uri", URLHelper.encodeSpecialChars(redirectUrl));
-        }};
+        var allScopes = new StringBuilder();
 
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(service);
-        stringBuilder.append("/oauth2/authorize?");
-        stringBuilder.append("client_id=");
-        stringBuilder.append(clientId);
-
-        for (var key : queryString.keySet()) {
-            stringBuilder.append("&").append(key).append("=").append(queryString.get(key));
+        for (var vsoScope : scope) {
+            allScopes.append(vsoScope.name());
+            allScopes.append("%20");
         }
+
+        StringBuilder stringBuilder = new StringBuilder()
+            .append(service)
+            .append("/oauth2/authorize?client_id=")
+            .append(clientId)
+            .append("&response_type=Assertion")
+            .append("&state=")
+            .append(state)
+            .append("&scope=")
+            .append(allScopes.toString().replaceAll("%20$", ""))
+            .append("&redirect_uri=")
+            .append(URLHelper.encodeSpecialChars(redirectUrl));
+
         return stringBuilder.toString();
     }
 
@@ -135,5 +145,4 @@ public class OAuthAccessTokenBuilder extends BaseRequestBuilder {
         // Overriding the requestAdapter as we don't want to send the access token yet to call the Api.
         return requestAdapter.send(reqInfo, AuthorizedToken.class);
     }
-
 }
