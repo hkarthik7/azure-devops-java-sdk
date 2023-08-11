604291f7c86b36e7bb1d4fe7db269b4362f078ac
Harish Karthic <harish.karthic@gmail.com>
2023-08-11 16:07:33 +0100

    Added functionality to get the request url and request information.

--- azd/src/main/java/org/azd/http/AzDResponseHandler.java
+++ azd/src/main/java/org/azd/http/AzDResponseHandler.java
@@ -6,8 +6,9 @@ import java.net.http.HttpResponse;
 
 public class AzDResponseHandler implements ResponseHandler {
     @Override
-    public <T> void setResponse(HttpResponse<T> response) {
-        AzDResponseHandler.response = new AzDResponse(response.statusCode(), response.headers(), response.body());
+    public <T> void setResponse(HttpResponse<T> response, RequestInformation requestInformation) {
+        AzDResponseHandler.response = new AzDResponse(response.statusCode(), response.headers(),
+                response.body(), response.request().uri().toString(), requestInformation);
     }
 
     public static AzDResponse getResponse() {
