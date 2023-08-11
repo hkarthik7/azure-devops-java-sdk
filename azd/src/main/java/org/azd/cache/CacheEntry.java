5136686a528e955283c9d9c11c2cd005cda2c0fb
Harish Karthic <harish.karthic@gmail.com>
2023-08-11 16:17:37 +0100

    Minor update

--- azd/src/main/java/org/azd/cache/CacheEntry.java
+++ azd/src/main/java/org/azd/cache/CacheEntry.java
@@ -1,20 +1,19 @@
 package org.azd.cache;
 
 public class CacheEntry<V> {
-    private final V value;
-    private final long expirationTimeMillis;
-
-    CacheEntry(V value, long ttlMillis) {
+    public CacheEntry(V value, long ttlMillis) {
         this.value = value;
         this.expirationTimeMillis = System.currentTimeMillis() + ttlMillis;
     }
 
-    boolean isExpired() {
+    public boolean isExpired() {
         return System.currentTimeMillis() > expirationTimeMillis;
     }
 
-    V getValue() {
+    public V getValue() {
         return value;
     }
 
+    private final V value;
+    private final long expirationTimeMillis;
 }
