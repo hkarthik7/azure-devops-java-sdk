685984a07da00c791a4fe73975565d275e383b46
Harish Karthic <harish.karthic@gmail.com>
2023-08-11 16:09:00 +0100

    Added request information

--- azd/src/main/java/org/azd/interfaces/ResponseHandler.java
+++ azd/src/main/java/org/azd/interfaces/ResponseHandler.java
@@ -1,7 +1,9 @@
 package org.azd.interfaces;
 
+import org.azd.http.RequestInformation;
+
 import java.net.http.HttpResponse;
 
 public interface ResponseHandler {
-    <T> void setResponse(HttpResponse<T> response);
+    <T> void setResponse(HttpResponse<T> response, RequestInformation requestInformation);
 }
