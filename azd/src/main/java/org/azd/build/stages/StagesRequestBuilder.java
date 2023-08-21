e99f98c4863c944d28f443b4e808ade57598e349
Harish Karthic <harish.karthic@gmail.com>
2023-08-21 13:24:54 +0100

    Added support for yaml, stages and timeline in Build Api

---
+++ azd/src/main/java/org/azd/build/stages/StagesRequestBuilder.java
@@ -0,0 +1,51 @@
+package org.azd.build.stages;
+
+import org.azd.common.ApiVersion;
+import org.azd.enums.StageUpdateType;
+import org.azd.exceptions.AzDException;
+import org.azd.interfaces.AccessTokenCredential;
+import org.azd.interfaces.RequestAdapter;
+import org.azd.utils.BaseRequestBuilder;
+
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Provides functionality to work with Build Stages Api.
+ */
+public class StagesRequestBuilder extends BaseRequestBuilder {
+    /**
+     * Instantiates a new request builder instance and sets the default values.
+     * @param accessTokenCredential Authentication provider {@link AccessTokenCredential}.
+     * @param requestAdapter The request adapter to execute the requests.
+     */
+    public StagesRequestBuilder(AccessTokenCredential accessTokenCredential, RequestAdapter requestAdapter) {
+        super(accessTokenCredential, requestAdapter, "build/builds", ApiVersion.BUILD_STAGE);
+    }
+
+    /**
+     * Update a build stage.
+     * @param updateStageRequest UpdateStageRequest object to update the build stage.
+     * @throws AzDException Default Api Exception handler.
+     */
+    public CompletableFuture<Void> update(UpdateStageRequest updateStageRequest)
+            throws AzDException {
+        var body = new HashMap<String, Object>() {{
+            put("forceRetryAllJobs", updateStageRequest.forceRetryAllJobs);
+            put("state", updateStageRequest.state.name());
+        }};
+
+        var reqInfo = toPatchRequestInformation(body);
+        reqInfo.serviceEndpoint = reqInfo.serviceEndpoint + "/" + updateStageRequest.buildId + "/stages/"
+                + updateStageRequest.stageReferenceName;
+
+        return requestAdapter.sendPrimitiveAsync(reqInfo);
+    }
+
+    public static class UpdateStageRequest {
+        public int buildId;
+        public String stageReferenceName;
+        public Boolean forceRetryAllJobs;
+        public StageUpdateType state;
+    }
+}
