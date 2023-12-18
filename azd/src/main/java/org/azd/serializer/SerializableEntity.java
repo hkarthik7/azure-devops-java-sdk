21874ac887c5e5e5a1107d35b3b33315456a5872
Harish Karthic <harish.karthic@gmail.com>
2023-12-18 06:20:16 +0000

    Added `getContinuationToken` method

--- azd/src/main/java/org/azd/serializer/SerializableEntity.java
+++ azd/src/main/java/org/azd/serializer/SerializableEntity.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.azd.exceptions.AzDException;
+import org.azd.handlers.DefaultResponseHandler;
+import org.azd.http.ApiResponse;
 import org.azd.interfaces.SerializerContext;
 import org.azd.utils.InstanceFactory;
 
@@ -34,6 +36,17 @@ public abstract class SerializableEntity {
         } catch (AzDException ignored) { }
 
         return res;
+    }
+
+    /**
+     * Retrieves the continuation token from response header. This can then be used for getting the paginated response.
+     * Note that not all the Apis return continuation token in the headers. If the header value "x-ms-continuationtoken"
+     * is present then it will be returned or an empty string will be returned.
+     * @return String value.
+     */
+    @JsonIgnore
+    public String getContinuationToken() {
+        return DefaultResponseHandler.getResponse().getContinuationToken();
     }
 
     @JsonIgnore
