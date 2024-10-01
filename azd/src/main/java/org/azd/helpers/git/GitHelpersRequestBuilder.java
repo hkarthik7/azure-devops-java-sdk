b4a8eaf887758d14e7d78f79f37b8895983fe485
Harish Karthic <harish.karthic@gmail.com>
2024-10-01 07:04:24 +0100

    Added new helper methods

--- azd/src/main/java/org/azd/helpers/git/GitHelpersRequestBuilder.java
+++ azd/src/main/java/org/azd/helpers/git/GitHelpersRequestBuilder.java
@@ -6,6 +6,7 @@ import org.azd.exceptions.AzDException;
 import org.azd.git.GitBaseRequestBuilder;
 import org.azd.git.types.*;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -121,5 +122,97 @@ public class GitHelpersRequestBuilder extends GitBaseRequestBuilder {
         }
 
         throw new AzDException("Fork sync request did not complete in time : " + gitForkSyncRequest.getStatus());
+    }
+
+    /**
+     * Request that another repository's refs be fetched into this one. It syncs two existing forks.
+     *
+     * @param repositoryName     The name or ID of the repository.
+     * @param sourceCollectionId Team Project Collection ID of the collection for the repository.
+     * @param sourceProjectId    Team Project ID of the project for the repository.
+     * @param sourceRepositoryId ID of the repository.
+     * @param includeLinks       Set to true to include links to items.  Default is false.
+     * @return GitForkSyncRequest Object {@link GitForkSyncRequest}
+     * @throws AzDException Default Api Exception handler.
+     **/
+    public GitForkSyncRequest createForkSyncRequest(String repositoryName, String sourceCollectionId, String sourceProjectId,
+                                                    String sourceRepositoryId, Boolean includeLinks) throws AzDException {
+
+        var source = new GlobalGitRepositoryKey();
+        source.setCollectionId(sourceCollectionId);
+        source.setProjectId(sourceProjectId);
+        source.setRepositoryId(sourceRepositoryId);
+
+        var forkSyncRequests = new GitForkSyncRequestParameters();
+        forkSyncRequests.source = source;
+
+        return forks().create(forkSyncRequests, repositoryName, r -> r.queryParameters.includeLinks = includeLinks);
+    }
+
+
+    /**
+     * Request that another repository's refs be fetched into this one. It syncs two existing forks.
+     *
+     * @param repositoryName The name or ID of the repository.
+     * @param collectionId   Team Project Collection ID of the collection for the repository.
+     * @param projectId      Team Project ID of the project for the repository.
+     * @param repositoryId   ID of the repository.
+     * @param sourceRef      The source ref to copy. For example, refs/heads/main.
+     * @param targetRef      The target ref to update. For example, refs/heads/main.
+     * @param includeLinks   Set to true to include links to items.  Default is false.
+     * @return GitForkSyncRequest Object {@link GitForkSyncRequest}
+     * @throws AzDException Default Api Exception handler.
+     **/
+    public GitForkSyncRequest createForkSyncRequest(String repositoryName, String collectionId, String projectId,
+                                                    String repositoryId, String sourceRef, String targetRef,
+                                                    Boolean includeLinks) throws AzDException {
+
+        var source = new GlobalGitRepositoryKey();
+        source.setCollectionId(collectionId);
+        source.setProjectId(projectId);
+        source.setRepositoryId(repositoryId);
+
+        var sourceToTargetRef = new SourceToTargetRef();
+        sourceToTargetRef.setSourceRef(sourceRef);
+        sourceToTargetRef.setTargetRef(targetRef);
+
+        var forkSyncRequests = new GitForkSyncRequestParameters();
+        forkSyncRequests.source = source;
+        forkSyncRequests.sourceToTargetRefs = List.of(sourceToTargetRef);
+
+        return forks().create(forkSyncRequests, repositoryName, r -> r.queryParameters.includeLinks = includeLinks);
+    }
+
+    /**
+     * Get a specific fork sync operation's details.
+     *
+     * @param repositoryName The name or ID of the repository.
+     * @param operationId    OperationId of the sync request.
+     * @param includeLinks   Set to true to include links to items.  Default is false.
+     * @return GitForkSyncRequest Object {@link GitForkSyncRequest}
+     * @throws AzDException Default Api Exception handler.
+     **/
+    public GitForkSyncRequest getForkSyncRequest(String repositoryName, int operationId, boolean includeLinks)
+            throws AzDException {
+
+        return forks().get(repositoryName, operationId, r -> r.queryParameters.includeLinks = includeLinks);
+    }
+
+    /**
+     * Retrieve all requested fork sync operations on this repository.
+     *
+     * @param repositoryName   The name or ID of the repository.
+     * @param includeLinks     Set to true to include links to items.  Default is false.
+     * @param includeAbandoned Set to true to include abandoned requests.  Default is false.
+     * @return GitForkSyncRequests Object {@link GitForkSyncRequests}
+     * @throws AzDException Default Api Exception handler.
+     **/
+    public GitForkSyncRequests getForkSyncRequests(String repositoryName, boolean includeLinks, boolean includeAbandoned)
+            throws AzDException {
+        return forks().get(repositoryName, r ->
+        {
+            r.queryParameters.includeLinks = includeLinks;
+            r.queryParameters.includeAbandoned = includeAbandoned;
+        });
     }
 }
