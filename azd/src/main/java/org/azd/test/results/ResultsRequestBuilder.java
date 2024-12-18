d5ef27393e615cece3e495cc1bb261e94bcbd37f
Harish Karthic <harish.karthic@gmail.com>
2024-12-18 14:31:21 +0000

    Added `async` methods

--- azd/src/main/java/org/azd/test/results/ResultsRequestBuilder.java
+++ azd/src/main/java/org/azd/test/results/ResultsRequestBuilder.java
@@ -1,16 +1,14 @@
 package org.azd.test.results;
 
 import org.azd.abstractions.BaseRequestBuilder;
-import org.azd.abstractions.QueryParameter;
 import org.azd.authentication.AccessTokenCredential;
 import org.azd.common.ApiVersion;
-import org.azd.enums.TestRunPublishContext;
-import org.azd.enums.TestRunState;
 import org.azd.exceptions.AzDException;
-import org.azd.test.types.*;
+import org.azd.test.types.TestCaseResult;
+import org.azd.test.types.TestCaseResults;
+import org.azd.test.types.TestRuns;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 
 /**
  * Provides functionality to work with Test results Api.
@@ -26,6 +24,67 @@ public class ResultsRequestBuilder extends BaseRequestBuilder {
         super(organizationUrl, accessTokenCredential, "test", "4637d869-3a76-4468-8057-0bb02aa385cf", ApiVersion.TEST_RESULTS);
     }
 
+    /**
+     * Get a test case results for a test run.
+     *
+     * @param runId ID of the run to get.
+     * @param testCaseResultId ID of the test case result to get.
+     * @return TestCaseResult Object {@link TestCaseResult}
+     * @throws AzDException Default Api Exception handler.
+     **/
+    public CompletableFuture<TestCaseResult> getAsync(int runId, int testCaseResultId) throws AzDException {
+        return builder()
+                .serviceEndpoint("runId", runId)
+                .serviceEndpoint("testCaseResultId", testCaseResultId)
+                .build()
+                .executeAsync(TestCaseResult.class);
+    }
+
+    /**
+     * Get a list of test results by run ID.
+     *
+     * @param runId ID of the run.
+     * @return Collection of TestCaseResults Object {@link TestRuns}
+     * @throws AzDException Default Api Exception handler.
+     **/
+    public CompletableFuture<TestCaseResults> listAsync(int runId) throws AzDException {
+        return builder()
+                .serviceEndpoint("runId", runId)
+                .build()
+                .executeAsync(TestCaseResults.class);
+    }
+
+    /**
+     * Create test results by run ID.
+     *
+     * @param runId   ID of the run to update.
+     * @param testCaseResults Test case results object to update.
+     * @return Test case results object {@link TestCaseResults}
+     * @throws AzDException Default Api Exception handler.
+     */
+    public CompletableFuture<TestCaseResults> createAsync(int runId, TestCaseResults testCaseResults) throws AzDException {
+        return builder()
+                .POST(testCaseResults.getResults())
+                .serviceEndpoint("runId", runId)
+                .build()
+                .executeAsync(TestCaseResults.class);
+    }
+
+    /**
+     * Update test results by run ID.
+     *
+     * @param runId   ID of the run to update.
+     * @param testCaseResults Test case results object to update.
+     * @return Test case results object {@link TestCaseResults}
+     * @throws AzDException Default Api Exception handler.
+     */
+    public CompletableFuture<TestCaseResults> updateAsync(int runId, TestCaseResults testCaseResults) throws AzDException {
+        return builder()
+                .PATCH(testCaseResults.getResults())
+                .serviceEndpoint("runId", runId)
+                .build()
+                .executeAsync(TestCaseResults.class);
+    }
 
     /**
      * Get a test case results for a test run.
